ab934b04fa40fd7184743860b9173927b86dc113
yonghuijiang <1767247871@qq.com>
2020-06-24 16:00:24 +0800

    Update docs

--- source/_posts/submit.sh
+++ source/_posts/submit.sh
@@ -8,12 +8,11 @@ git config user.email "1767247871@qq.com"
 echo 添加文件至暂存区
 git add .
 echo ===========下面为本次需要提交的内容==========
-echo $(git ls-files)
 echo ==========开始提交代码=========
 git commit -m "Update docs"
 echo ===========提交daima1结束,准备拉取代码============
 git pull
-echo ============拉取代码代码结束，准备推送代码到远程分支 "\n" $(git branch) ============
+echo ============拉取代码代码结束，准备推送代码到远程分支============
 git push
 echo ========推送到远程分支结束==========
 
